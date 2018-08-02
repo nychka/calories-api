e91ed54b17616cb6f23520a9f3af619ce2c0aa21
Yaroslav Nychka <nychka93@gmail.com>
2018-08-02 23:23:33 +0300

    add CategoriesController

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,18 @@
 class Category < ApplicationRecord
   store_accessor :lang
   has_many :products
+  paginates_per 20
+
+  default_scope { order(created_at: :desc) }
+  scope :max_count, ->(max) { limit(max) unless max.nil? }
+
+  class << self
+    def filter(params)
+      if params[:page].present?
+        page(params[:page])
+      else
+        max_count(params[:limit])
+      end
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: 'users/sessions' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :products
+  resources :categories
 end
